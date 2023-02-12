1690fe6b5743fe3e54769631bae4cfa1d89a6f28
Rayan <bedair.rayan@gmail.com>
2023-02-12 11:16:45 +0100

    Added cases for crossed words

--- challenges/1.js
+++ challenges/1.js
@@ -1,5 +1,44 @@
-var inputs = document.querySelectorAll('input[type=text]');
+var inputs = document.querySelectorAll('input[type=text]'), input_class;
+var tables = document.querySelectorAll('table');
 
-[].forEach.call(inputs, function (input) {
-    input.style['inline-size'] = (3 * input.maxLength).toString() + 'ch';
-});
+var td = document.createElement('td'), cell;
+var tr = document.createElement('tr'), row;
+
+function MakeTable(table, length) {
+
+    row = table.insertRow()
+    for (var i = 0; i < length; i++) {
+        cell = row.insertCell();
+        cell.innerHTML = 'a';
+
+    }
+
+}
+
+function MakeVerticalTable(table, length) {
+
+    for (var i = 0; i < length; i++) {
+        row = table.insertRow();
+        cell = row.insertCell();
+        cell.innerHTML = 'z';
+
+    }
+
+}
+
+
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].style['inline-size'] = (3 * inputs[i].maxLength).toString() + 'ch';
+    isVerticalTable = inputs[i].getAttribute('class') === 'vertical';
+
+    isVerticalTable ? MakeVerticalTable(tables[i], inputs[i].maxLength) : MakeTable(tables[i], inputs[i].maxLength);
+
+}
+
+// [].forEach.call(inputs, function (input) {
+//     input.style['inline-size'] = (3 * input.maxLength).toString() + 'ch';
+
+//     for (var i = 0; i < input.maxLength; i++) {
+//         table.insertCell();
+//     }
+// });
