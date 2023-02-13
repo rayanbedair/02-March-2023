b3a1710017271f7863b1b64970eaed284b7b2ace
Rayan <bedair.rayan@gmail.com>
2023-02-13 12:35:39 +0100

    Changed order of functions

--- challenges/1.js
+++ challenges/1.js
@@ -3,6 +3,20 @@ var tables = document.querySelectorAll('table'), table;
 
 var isVerticalTable, cells, cell, rows, row;
 
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].style['inline-size'] = (3 * inputs[i].maxLength).toString() + 'ch';
+    isVerticalTable = inputs[i].getAttribute('class') === 'vertical';
+
+    // Emptying the text when loading the page
+    inputs[i].value = '';
+
+    // Adding parameter for the 'input' event listener
+    inputs[i].table = tables[i];
+
+    isVerticalTable ? MakeVerticalTable(tables[i], inputs[i].maxLength) : MakeTable(tables[i], inputs[i].maxLength);
+    isVerticalTable ? inputs[i].addEventListener('input', UpdateVerticalTable) : inputs[i].addEventListener('input', UpdateTable)
+}
+
 function MakeTable(table, length) {
 
     row = table.insertRow()
@@ -19,22 +33,6 @@ function MakeVerticalTable(table, length) {
         cell = row.insertCell();
         cell.innerHTML = '_';
     }
-}
-
-
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].style['inline-size'] = (3 * inputs[i].maxLength).toString() + 'ch';
-    isVerticalTable = inputs[i].getAttribute('class') === 'vertical';
-
-    // Emptying the text when loading the page
-    inputs[i].value = '';
-
-    // Adding parameter for the 'input' event listener
-    inputs[i].table = tables[i];
-
-    isVerticalTable ? MakeVerticalTable(tables[i], inputs[i].maxLength) : MakeTable(tables[i], inputs[i].maxLength);
-    isVerticalTable ? inputs[i].addEventListener('input', UpdateVerticalTable) : inputs[i].addEventListener('input', UpdateTable)
-
 }
 
 function UpdateTable(event) {
