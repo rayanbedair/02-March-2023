16c0f6610b777f4935d1ffa15ff1f4a18ad4aa1d
Rayan <bedair.rayan@gmail.com>
2023-02-19 18:01:43 +0100

    Added js to handle click events

---
+++ menu/shop.js
@@ -0,0 +1,26 @@
+var images = document.querySelectorAll('img'), image;
+var promptHint, imgId, solutions, jsonStr;
+
+fetch('solutions.json')
+    .then(response => response.json())
+    .then(jsonResponse => {
+        solutions = eval(jsonResponse);
+
+        for (image of images) {
+
+            if (!solutions[image.id].found) {
+                image.addEventListener('click', PromptHint, false);
+            }
+        };
+    });
+
+
+function PromptHint(event) {
+    promptHint = prompt('Indice : ');
+    imgId = event.srcElement.id;
+
+    if (promptHint === solutions[imgId].name) {
+        event.srcElement.setAttribute('style', 'filter: blur(0px);');
+    }
+
+}
