bdeac670222fdadc5443528c9f1190d9883dc856
Rayan <bedair.rayan@gmail.com>
2023-02-26 12:33:21 +0100

    Remade from scratch table filling algorithm

--- challenges/1.js
+++ challenges/1.js
@@ -1,81 +1,68 @@
-var inputs = document.querySelectorAll('input[type=text]');
-var tables = document.querySelectorAll('table'), table;
+// This is a list of alert popups with the text to say when Chacha will select an element of the legend
+var prompts = [
+    "Alors, pour le premier mot, ça serait :",
+    "Et le 2eme mot est :",
+    "Para el numero 3, la palabra es :",
+    "Khobz pour le 4 :",
+    "5 :",
+    "pOUR LE NUMÉRO SISSE :",
+    "Ah donc tu es déjà au dernier mot toi ? Bah donne le, vas-y :"
+];
 
-var isVerticalTable, cells, cell, rows, row;
+// This are coordinates for the words:
+// The key of this map is the index of the word, in the legend list
+// The values correspond to the indexes of the beggining and of the end of the word, in the table
+// For example, the 1st word will take all cells of the table of the 3rd row (index 2), between the indexes 1 and 9.
+var coordinatesOfWords = {
+    1: [[2, 1], [2, 9]],
+    2: [[1, 8], [5, 8]],
+    3: [[5, 3], [5, 8]],
+    4: [[5, 5], [9, 5]],
+    5: [[5, 7], [10, 7]],
+    6: [[7, 3], [7, 8]],
+    7: [[10, 7], [10, 12]]
+}
 
-// Map that holds as keys the index of the table and as value the index of the cell item (ex: 0:3 means the 3rd cell of the 0th table must be in red)
-var lettersInRed = { 0: 3, 1: 0, 2: 1, 3: 2, 5: 1, 6: 4 }, letterInRed;
+var tbody = document.getElementsByTagName('tbody')[0];
 
-// Map that holds as keys the index of the table and as value the place of the letter in the word (ex: 0:2 means the red letter in the 0th table has to be displayed as '2')
-var hintsOrder = { 0: 2, 1: 5, 2: 1, 3: 3, 5: 6, 6: 4 };
 
-for (var i = 0; i < inputs.length; i++) {
-    isVerticalTable = inputs[i].getAttribute('class') === 'vertical';
-    inputs[i].style['inline-size'] = isVerticalTable ? (2 * inputs[i].maxLength - 2.75).toString() + 'ch' : (3 * inputs[i].maxLength + 4).toString() + 'ch';
-
-    // Emptying the text when loading the page
-    inputs[i].value = '';
-
-    // Adding parameters for the 'input' event listener
-    inputs[i].table = tables[i];
-    inputs[i].index = i;
-
-    isVerticalTable ? MakeVerticalTable(tables[i], inputs[i].maxLength) : MakeTable(tables[i], inputs[i].maxLength);
-    isVerticalTable ? inputs[i].addEventListener('input', UpdateVerticalTable) : inputs[i].addEventListener('input', UpdateTable)
-
-    if (lettersInRed[i] !== undefined) {
-
-        rows = tables[i].tBodies[0].rows;
-        letterInRed = lettersInRed[i];
-
-        cell = isVerticalTable ? rows[letterInRed].cells[0] : rows[0].cells[letterInRed];
-        cell.classList.add('red_letter');
-    }
+for (var i = 1; i < 8; i++) {
+    document.getElementById(i).addEventListener('click', FillTable, false);
 }
 
 
-function MakeTable(table, length) {
+function FillTable(e) {
 
-    row = table.insertRow()
-    for (var i = 0; i < length; i++) {
-        cell = row.insertCell();
-        cell.innerHTML = '_';
+    var id = e.target.id;
+    var response = prompt(prompts[id - 1]).toUpperCase();
+    var wordIndexes = coordinatesOfWords[id];
+    var wordLength = 1 + (wordIndexes[1][0] - wordIndexes[0][0]) + (wordIndexes[1][1] - wordIndexes[0][1]);
+
+    if (response === null) return;
+    if (response === "") return;
+
+    for (var i = response.length; i < wordLength; i++) {
+        response += "_";
     }
-}
 
-function MakeVerticalTable(table, length) {
 
-    for (var i = 0; i < length; i++) {
-        row = table.insertRow();
-        cell = row.insertCell();
-        cell.innerHTML = '_';
+    for (var i = wordIndexes[0][0], k = 0; i <= wordIndexes[1][0]; i++)  {
+        for (var j = wordIndexes[0][1]; j <= wordIndexes[1][1]; j++) {
+
+            tbody.rows[i].cells[j].innerText = response[k];
+            k++;
+        }
     }
-}
 
-function UpdateTable(event) {
+    console.log(k, response.length);
+    // If we've reached both of the boundaries, we've properly filled the table.
+    if (response.length >= wordLength) return;
+    
+    for (var i = wordIndexes[0][0], k = 0; i <= wordIndexes[1][0]; i++) {
+        for (var j = wordIndexes[0][1]; j <= wordIndexes[1][1]; j++) {
 
-    cells = event.currentTarget.table.rows[0].cells;
-    index = event.currentTarget.index;
-
-    for (i = 0; i < cells.length && i < event.target.value.length; i++) {
-        // Checking if the letter to be replaced is a hint or not
-        cells[i].innerText = cells[i].classList.contains('red_letter') ? hintsOrder[index].toString() : event.target.value[i].toUpperCase();
+            tbody.rows[i].cells[j].innerText = "_";
+        }
     }
-    for (i = event.target.value.length; i < cells.length; i++) {
-        cells[i].innerText = '_';
-    }
-}
 
-function UpdateVerticalTable(event) {
-
-    rows = event.currentTarget.table.rows;
-    index = event.currentTarget.index;
-
-    for (i = 0; i < rows.length && i < event.target.value.length; i++) {
-        // Checking if the letter to be replaced is a hint or not
-        rows[i].cells[0].innerText = rows[i].cells[0].classList.contains('red_letter') ? hintsOrder[index].toString() : event.target.value[i].toUpperCase();
-    }
-    for (i = event.target.value.length; i < rows.length; i++) {
-        rows[i].cells[0].innerText = '_';
-    }
 }
