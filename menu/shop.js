35fa09f7a6e7329fd32979f7104ce0daa4e86495
Rayan <bedair.rayan@gmail.com>
2023-03-01 23:50:53 +0100

    Added local storage to keep in memory good solutions

--- menu/shop.js
+++ menu/shop.js
@@ -1,24 +1,35 @@
 var images = document.querySelectorAll('img'), image;
 var promptHint, imgId, solutions, jsonStr;
 
+
 fetch('solutions.json')
     .then(response => response.json())
     .then(jsonResponse => {
         solutions = eval(jsonResponse);
 
-        for (image of images) {
-            image.addEventListener('click', PromptHint, false);
-        }
+
     });
 
+for (var i = 0; i < images.length; i++) {
+
+    images[i].addEventListener('click', PromptHint, false);
+
+    // If the hint has already been found
+    if (localStorage.getItem(i + 1) === "ok") {
+        images[i].setAttribute('style', 'filter: none;');
+        images[i].removeEventListener('click', PromptHint, false);
+    }
+}
 
 function PromptHint(event) {
     promptHint = prompt('Indice : ');
     imgId = event.srcElement.id;
 
+    // If the hint is correct
     if (promptHint === solutions[imgId]) {
         event.srcElement.setAttribute('style', 'filter: none;');
         event.srcElement.removeEventListener('click', PromptHint, false);
+        localStorage.setItem(imgId, 'ok');
     }
 
 }
